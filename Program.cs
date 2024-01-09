79dfd58a0a4cdbb428fb1f3c3b2b966e3da5ef6f
Gordon Beeming <gpg@gordonbeeming.com>
2024-01-10 00:00:47 +1000

    back to basic string inter

--- Program.cs
+++ Program.cs
@@ -1,11 +1,8 @@
 ﻿var name = "Gordon";
-FormattableString stringLiteral = $@"
+var stringLiteral = $@"
 Hi { name },
 
 Strings are cool!
 ";
 
-Console.WriteLine(stringLiteral.Format);
-System.Console.WriteLine(stringLiteral.ArgumentCount);
-
-System.Console.WriteLine(stringLiteral.GetArguments()[0]);
+Console.WriteLine(stringLiteral);
